3d72c7fcf22d36f27dd1ad5bcbb71a62d2718551
Eric deRegt <ericderegt@gmail.com>
2015-05-26 12:14:51 -0400

    add routes and categories api

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,33 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Category.delete_all
+# Category.delete_all
+Event.delete_all
 
-Category.create({category: "big data"})
-Category.create({category: "fitness"})
-Category.create({category: "user experience"})
-Category.create({category: "startups"})
-Category.create({category: "animals"})
-Category.create({category: "artificial intelligence"})
-Category.create({category: "innovation"})
-Category.create({category: "photography"})
-Category.create({category: "food"})
-Category.create({category: "ruby"})
-Category.create({category: "programming"})
-Category.create({category: "javascript"})
+# Category.create({category: "big data"})
+# Category.create({category: "fitness"})
+# Category.create({category: "user experience"})
+# Category.create({category: "startups"})
+# Category.create({category: "animals"})
+# Category.create({category: "artificial intelligence"})
+# Category.create({category: "innovation"})
+# Category.create({category: "photography"})
+# Category.create({category: "food"})
+# Category.create({category: "ruby"})
+# Category.create({category: "programming"})
+# Category.create({category: "javascript"})
+
+    # t.string   "name"
+    # t.string   "location"
+    # t.datetime "date"
+    # t.integer  "time"
+    # t.string   "attendee_limit"
+    # t.string   "location_name"
+    # t.string   "street"
+    # t.string   "city"
+    # t.string   "state"
+    # t.integer  "zip"
+    # t.string   "description"
+    # t.string   "categories"
+
+
