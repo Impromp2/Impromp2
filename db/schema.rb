326814ea1bb073113760144448281f9580f94df1
Eric deRegt <ericderegt@gmail.com>
2015-05-26 10:32:03 -0400

    delete users table

--- db/schema.rb
+++ db/schema.rb
@@ -49,13 +49,4 @@ ActiveRecord::Schema.define(version: 20150526141830) do
     t.datetime "updated_at",     null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
 end
