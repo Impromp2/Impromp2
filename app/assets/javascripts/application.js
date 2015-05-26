3c9d87e929ec3982559de1bf3dee2f8e97eb4555
John Kinney <johnkinney77@gmail.com>
2015-05-26 15:31:52 -0400

    moved some code to another file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,12 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-$(function(){
-  $('#drop-down').on('mouseenter', function(){
-    $('#nav-right-dropdown').show()
-  });
-  $('#drop-down').on('mouseleave', function(){
-    $('#nav-right-dropdown').hide()
-  });
-
-});
---
+++ app/assets/javascripts/nav-dropdown.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('#drop-down').on('mouseenter', function(){
+    $('#nav-right-dropdown').show()
+  });
+  $('#drop-down').on('mouseleave', function(){
+    $('#nav-right-dropdown').hide()
+  });
+});
