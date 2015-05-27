a29a357add39b36f3f5090b0b095019c60343298
Gabby Losch <glosch00@gmail.com>
2015-05-27 13:53:37 -0400

    changed var name in UsersController from @current_user to session_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
   # Used to create the backbone model for the logged in User
   def show
     # Assigns a user's availabilities to the current user object
-    @current_user = User.find(current_user.id).to_json(:include => :availabilities)
-    render json: @current_user
+    session_user = User.find(current_user.id).to_json(:include => :availabilities)
+    render json: session_user
   end
 
 
