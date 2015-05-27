3d052d41f425e44e6b74b3101f80e7be706dde0e
John Kinney <johnkinney77@gmail.com>
2015-05-27 12:01:22 -0400

    fixed associations issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :availabilities,
-  has_many :categories,
-  belongs_to :availabilities,
-  belongs_to :categories,
+  has_many :availabilities
+  has_many :categories
+  has_many :events
+  belongs_to :availabilities
+  belongs_to :categories
+  belongs_to :events
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
