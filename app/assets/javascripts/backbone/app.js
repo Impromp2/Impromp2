5bbe95b075fd0aebfbaebfce88bcbaee5f382616
Gabby Losch <glosch00@gmail.com>
2015-05-28 11:39:57 -0400

    created instance of availabilityCollection and fetching it in Backbone app.js

--- app/assets/javascripts/backbone/app.js
+++ app/assets/javascripts/backbone/app.js
@@ -4,16 +4,19 @@ var eventCollection;
 
 Impromp2App.initialize = function(){
   var eventCollection = new Impromp2App.Collections.EventCollection();
+  var availabilityCollection = new Impromp2App.Collections.AvailabilityCollection();
   var listView = new Impromp2App.Views.EventListView({
-        collection: eventCollection,
-        el: $('#main-content')
+    collection: eventCollection,
+    el: $('#main-content')
   });
 
   Impromp2App.currentUser = new Impromp2App.Models.User();
   Impromp2App.currentUser.fetch();
 
   eventCollection.fetch();
+  availabilityCollection.fetch();
   console.log(Impromp2App.currentUser);
+  console.log(availabilityCollection);
   // Impromp2App.availabilities = Impromp2App.currentUser.attributes.availabilities;
 };
 
