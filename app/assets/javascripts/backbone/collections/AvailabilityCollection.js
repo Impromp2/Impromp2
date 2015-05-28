f0beb48f5ddf86a1ab93fd7bea5ef0d0fb035afd
Gabby Losch <glosch00@gmail.com>
2015-05-28 11:35:07 -0400

    fixed api/availabilities route in Backbone Collection

--- app/assets/javascripts/backbone/collections/AvailabilityCollection.js
+++ app/assets/javascripts/backbone/collections/AvailabilityCollection.js
@@ -2,10 +2,10 @@ var Impromp2App = Impromp2App || { Models: {}, Collections: {}, Views: {} };
 
 Impromp2App.Collections.AvailabilityCollection = Backbone.Collection.extend({
   initialize: function(options){
-    
+
   },
   model: Impromp2App.Models.Availability,
   url: function(){
-   return 'api/availability';
+   return 'api/availabilities';
   }
 });
