a3af09ebcc3c4a6abd071c2d38b081dacd2e2265
Eric deRegt <ericderegt@gmail.com>
2015-05-28 15:57:18 -0400

    event list view sorts now

--- app/assets/javascripts/backbone/views/events/EventListView.js
+++ app/assets/javascripts/backbone/views/events/EventListView.js
@@ -1,24 +1,20 @@
 var Impromp2App = Impromp2App || { Models: {}, Collection: {}, Views: {} };
 
 Impromp2App.Views.EventListView = Backbone.View.extend({
- initialize: function(){
-   this.listenTo(this.collection, 'add', this.render);
- },
- sortEvents: function(){
-    var sortedEvents = _.groupBy(this.collection.models, )
-
-    return sortedEvents
-
- },
- render: function(modelFromCollection){
-
+  initialize: function(){
+    this.listenTo(this.collection, 'add', this.render);
+  },
+  render: function(modelFromCollection){
+    var sortedEvents = _.groupBy(this.collection.models, function(event){
+      return event.get('time'); 
+    })
 
     _.each(sortedEvents, function(day,events){
-        newEventView = 
+        console.log(day, events);
     })
 
 
-   var newEventView = new Impromp2App.Views.EventView({model: modelFromCollection});
-   this.$el.append(newEventView.render().$el);
- }
+   // var newEventView = new Impromp2App.Views.EventView({model: modelFromCollection});
+   // this.$el.append(newEventView.render().$el);
+  }
 });
