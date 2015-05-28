4e1e533e3f2595a69b92de731e86334b3e03759f
Gabby Losch <glosch00@gmail.com>
2015-05-28 18:15:04 -0400

    resolving merge conflict

--- app/assets/javascripts/backbone/app.js
+++ app/assets/javascripts/backbone/app.js
@@ -7,23 +7,15 @@ Impromp2App.initialize = function(){
 
   console.log('app initialized');
   var eventCollection = new Impromp2App.Collections.EventCollection();
-  // var availabilityCollection = new Impromp2App.Collections.AvailabilityCollection();
   var listView = new Impromp2App.Views.EventListView({
     collection: eventCollection,
     el: $('#main-content')
   });
-<<<<<<< HEAD
-=======
 
   var promise = eventCollection.fetch();
   promise.done(function(){
-    console.log('events fetched')
-  })
-  // availabilityCollection.fetch();
-
-  // console.log(availabilityCollection);
-  // Impromp2App.availabilities = Impromp2App.currentUser.attributes.availabilities;
->>>>>>> 96b2d96e7a8e569809eafcba776ccdfc178aeb28
+    console.log('events fetched');
+  });
 };
 
 
